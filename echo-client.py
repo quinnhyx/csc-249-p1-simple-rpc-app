e18dbfc71ae784ad8fdccf9a963cb70adaa8e7f8
quinnhyx <qhe@smith.edu>
2024-09-18 19:43:29 -0400

    concat+reverse

--- echo-client.py
+++ echo-client.py
@@ -1,18 +1,26 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
-MSG = "Hello, world"
 
-print("client starting - connecting to server at IP", HOST, "and port", PORT)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print(f"connection established, sending message '{MSG}'")
-    s.sendall(bytes(MSG, 'utf-8'))
-    print("message sent, waiting for reply")
-    data = s.recv(1024)
+def encode_request(operation, *args):
+    request = f"{operation} {' '.join(map(str, args))}"
+    return request.encode()
 
-print(f"Received response: '{data!r}' [{len(data)} bytes]")
-print("client is done!")
+def main():
+    print("client starting - connecting to server at IP", HOST, "and port", PORT)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        operation = input("Enter the operation (concat/reverse): ")
+        args = input("Enter words separated by space:").split()
+        request = encode_request(operation, *args)
+        s.sendall(request)
+        print("message sent, waiting for reply")
+        data = s.recv(1024)
+        print(f"Received: {data.decode()}")
+        s.close()
+
+if __name__ == "__main__":
+    main()
